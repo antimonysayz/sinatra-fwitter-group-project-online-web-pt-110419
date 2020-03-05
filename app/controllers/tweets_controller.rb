245f866393b95e4d6ee06f1592700e2ba5393e35
Anthony Mai <anthony.mai530@gmail.com>
2020-03-05 03:50:37 +0000

    signup page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,13 @@ class TweetsController < ApplicationController
     set :session_secret, "tweet_secret"
   end
 
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => [:email], :password => params[:password])
+      @user.save
+      @user.id = session[:user_id]
+      redirect to '/tweets'
+    end
+  end
+
+
+
 
 end
